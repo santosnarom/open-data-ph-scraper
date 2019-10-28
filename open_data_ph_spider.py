ce09d504f6466e352f6f821d0e62576a5c3cb8be
santosnarom <santosnarom@gmail.com>
2019-10-28 08:45:22 +0800

    add crawling up to the dataset page

--- open_data_ph_spider.py
+++ open_data_ph_spider.py
@@ -1,6 +1,8 @@
 import logging
 from urllib.parse import urlencode
 
+from bs4 import BeautifulSoup
+
 from scrapy import Spider, Request
 from scrapy.exceptions import CloseSpider
 
@@ -9,7 +11,7 @@ class OpenDataPhSpider(Spider):
     name = 'open_data_ph_spider'
     base_url = 'https://data.gov.ph/'
 
-    def __init__(self, keyword=None, ext=None, *args, **kwargs):
+    def __init__(self, keyword=None, *args, **kwargs):
         if not keyword:
             logging.error('Please provide a search keyword')
             raise CloseSpider()
@@ -19,8 +21,19 @@ class OpenDataPhSpider(Spider):
         self.search_url = '{}?{}'.format(self.base_url, urlencode(params))
 
     def start_requests(self):
-        yield Request(self.search_url, callback=self.handle_initial_response)
+        yield Request(self.search_url, callback=self.handle_search_response)
 
-    def handle_initial_response(self, response):
-        from scrapy.utils.response import open_in_browser
-        open_in_browser(response)
+    def handle_search_response(self, response):
+        soup = BeautifulSoup(response.body, 'lxml')
+        result_div = soup.find('div', attrs={'class': 'search-result-dataset'})
+
+        if result_div:
+            links = [header.a['href'] for header in result_div.find_all('h2')]
+            for link in links:
+                yield response.follow(link, callback=self.handle_dataset_page)
+        else:
+            logging.warning('No results found for %s', self.keyword)
+
+    def handle_dataset_page(self, response):
+        from scrapy.utils.response import open_in_browser; open_in_browser(response)
+        import pdb; pdb.set_trace()
